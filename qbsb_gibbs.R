f14d23dfe69862ba6f9283c41e7c7194d9128d03
Leo <leo.duan.work@gmail.com>
2020-06-04 15:46:02 -0400

    sync

--- qbsb_gibbs.R
+++ qbsb_gibbs.R
@@ -1,40 +1,41 @@
 rm(list=ls())
 
-rtbeta <- function(n, alpha, beta, a=0, b=1)
+rtbeta <- function(n, alpha, beta,a=0, b=1)
 {
   stopifnot(n > 0 & all(beta > 0) & all(alpha > 0))
   x <- runif(n)
   Fa <- pbeta(a, alpha, beta)
   Fb <- pbeta(b, alpha, beta)
-  y <- (1-x)*Fa + x*Fb
+  y <-  x*Fb
+  y[y<1E-16]=1E-16
   return(qbeta(y, alpha, beta))
 }
 
+# rtbeta(K, par1_beta, par2_beta,0,eps)
 
-
-rtbeta(100,1,100,0, 1E-8)
 
 
 setwd("~/git/sparse_dirichlet/")
 library(LaplacesDemon)
 
 K.true = 3
-K = 10  # original number of clusters
-N = 3000    # sample size
+K = 30  # original number of clusters
+N = 1000    # sample size
 y <- rnormm(N, p = c(0.3, 0.3, 0.4), mu = c(-3, 0, 4), sigma = c(1, 1, 1))
 
+# initialize the parameters
+mu = rnorm(K)
+sigma2 = 1/rgamma(K,2,1)
+hist(y,breaks = 100)
 
 w = rdirichlet(1, alpha = rep(1,K))
 s = 0.1
 
-u = rep(1,K)
+b = rep(1,K)
 beta = rbeta(K,1,s)
 beta
 
-mu = rnorm(K)
-sigma2 = 1/rgamma(K,2,1)
 
-hist(y,breaks = 100)
 
 ################################
 
@@ -49,7 +50,6 @@ updateC <- function(mu,sigma2,w){
   C<- t(apply(loglik + gumbel, 1,function(x){x==max(x)}))
   return(C)
 }
-
 
 
 updateMu <- function(C,sigma2){
@@ -82,81 +82,80 @@ updateSigma2<- function(C,mu){
   return(sigma2)
 }
 
-updateBeta<- function(C,u,  eps = 1E-12){
+updateBeta<- function(C, b, alpha_beta = 1 ,eps   = 1E-3){
   
   n_C <- colSums(C)
-  alpha_beta = 1
-  par1_beta = n_C + 1
-  par2_beta = (N - cumsum(n_C)) + alpha_beta
+  par1_beta = (N - cumsum(n_C)) + alpha_beta 
+  par2_beta = n_C + 1
   
   beta_1 = rbeta(K, par1_beta, par2_beta)
   beta_2 = rtbeta(K, par1_beta, par2_beta,0,eps)/eps
   
-  beta= beta_1*(u==1) + beta_2*(u!=1)
+  beta= beta_1*(b==1) + beta_2*(b!=1)
   
   # beta[K]=1E-8
   if(any(is.na(beta))){
     # print("beta err")
-    beta = updateBeta(C,u)
+    beta = updateBeta(C,b, alpha_beta,eps)
   }
   return(beta)
 }
 
 
-updateU<- function(C, beta,   eps = 1E-12) {
-  alpha_beta = 1
-  p_u = 1E-3
+updateB<- function(C, beta,   eps=1E-3, p_b = 1E-1,alpha_beta = 1) {
+  
   n_C <- colSums(C)
   n_gtK = N - cumsum(n_C)
   
-  choice1 = log(p_u) 
-  choice2 = log(1-p_u) + pbeta(eps, n_C+1, alpha_beta+ n_gtK,log.p =T) - log(eps) 
+  choice1 = log(p_b)
+  choice2 = log(1-p_b) + pbeta(eps, alpha_beta+ n_gtK, n_C+1, log.p =T) - alpha_beta * log(eps) 
   
   gumbel = -log(-log(runif(K*2,min = 0,max=1)))
   
   prob_choice = cbind(choice1,choice2)  + gumbel
-  u<- colSums((apply(prob_choice,1,function(x)x==max(x))) * c(1,eps))
-  return(u)
+  b<- colSums((apply(prob_choice,1,function(x)x==max(x))) * c(1,eps))
+  return(b)
 }
 
-updateW<- function(u,beta){
+
+updateW<- function(b,beta){
   
-  ubeta = u*beta
-  
-  w = ubeta * (cumprod(c(1, 1-ubeta))[1:K])
+  v = 1- b*beta
+  w = v * (cumprod(c(1, 1-v))[1:K])
 
   return(w)
   
 }
+
+
+
+
+eps = 1/N**(1.5)
 
 C<- updateC(mu,sigma2,w)
 C_label = colSums((t(C)*c(1:K)))
 n_C <- colSums(C)
 mu<- updateMu(C,sigma2)
 sigma2<- updateSigma2(C,mu)
-
-beta<- updateBeta(C,u)
-u<- updateU(C,beta)
-w<- updateW(u,beta) 
-
+beta<- updateBeta(C,b,eps = eps)
+b<- updateB(C, beta,eps=eps)
+w<- updateW(b,beta) 
 
 
-for (i in c(1:5000)){
+
+for (i in c(1:10000)){
+  
   C<- updateC(mu,sigma2,w)
   C_label = colSums((t(C)*c(1:K)))
   n_C <- colSums(C)
   mu<- updateMu(C,sigma2)
   sigma2<- updateSigma2(C,mu)
+  beta<- updateBeta(C,b,eps = eps)
+  b<- updateB(C, beta,eps=eps)
+  w<- updateW(b,beta) 
   
-  beta<- updateBeta(C,u)
-  u<- updateU(C,beta)
-  w<- updateW(u,beta) 
   if(i %% 100==0){
     print(n_C)
-    print(sum(u))
+    print(sum(b))
   }
 }
-
-sum(u)
-
-plot(w)
